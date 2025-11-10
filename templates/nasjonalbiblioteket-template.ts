077c91d15feaa87b0c1dba8e2c67746e190abc56
jorgenbry <72873986+jorgenbry@users.noreply.github.com>
2025-11-10 23:27:40 +0100

    fix spacing

--- templates/nasjonalbiblioteket-template.ts
+++ templates/nasjonalbiblioteket-template.ts
@@ -20,7 +20,7 @@ export const nasjonalbiblioteketTemplate = `<!DOCTYPE html>
         {{DEPARTMENT_ROW}}
         <!-- Row 2: Logo -->
         <tr>
-            <td style="padding: 0 12px 12px 24px; border: none;">
+            <td style="padding: 14px 0 24px 0; border: none;">
                 <img src="{{LOGO_URL}}" 
                      alt="{{LOGO_ALT}}" 
                      width="{{LOGO_WIDTH}}" 
